3ca0691b8bb2fd8aea7a6ad334a3b2d8711c13ff
Maciej Krüger <mkg20001@gmail.com>
2024-03-15 23:45:35 +0100

    cleanup

--- pi/nginx.nix
+++ pi/nginx.nix
@@ -24,14 +24,12 @@ in
     "radarr.oliver-koss.at" = h {
       locations."/" = {
         proxyPass = "http://localhost:7878/";
-        basicAuth."torrent4ever" = "4baes89joa47eiz8b6vj87iab4ienb6tzhv4kejgtr7be4av4nwtjk";
       };
     };
 
     "sonarr.oliver-koss.at" = h {
       locations."/" = {
         proxyPass = "http://localhost:8989/";
-        basicAuth."torrent4ever" = "4baes89joa47eiz8b6vj87iab4ienb6tzhv4kejgtr7be4av4nwtjk";
       };
     };
   };
