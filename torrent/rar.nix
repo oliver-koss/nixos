643efe513386ca6494a20fc6304c20b34611d406
Maciej Krüger <mkg20001@gmail.com>
2024-03-16 20:18:18 +0100

    reenable rr

--- torrent/rar.nix
+++ torrent/rar.nix
@@ -1,10 +1,10 @@
 {
-/*  services.jellyfin = {
+  services.jellyfin = {
     enable = true;
     openFirewall = true;
   };
 
   services.radarr.enable = true;
   services.sonarr.enable = true;
-  services.bazarr.enable = true;*/
+  services.bazarr.enable = true;
 }
