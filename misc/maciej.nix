eb3a31a317c68cfdfe458f39774d1486ae4389d1
Maciej Krüger <mkg20001@gmail.com>
2024-06-23 14:10:50 +0200

    add myself to docker group

--- misc/maciej.nix
+++ misc/maciej.nix
@@ -5,7 +5,7 @@ with lib;
 {
   users.users.maciej = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "dialout" "incus-admin" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "dialout" "incus-admin" "docker" ]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = splitString "\n"
       (builtins.readFile ./maciej.pub);
   };
