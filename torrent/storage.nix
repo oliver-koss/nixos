81f7acc8ee608d80fffe3b02ab705a13232d03fb
Maciej Krüger <mkg20001@gmail.com>
2024-07-19 17:33:00 +0200

    torrentz: add job to dedup storage

--- torrent/storage.nix
+++ torrent/storage.nix
@@ -1,6 +1,15 @@
-{
+{ pkgs, ... }: {
   fileSystems."/storage" =
     { device = "torrentz";
       fsType = "zfs";
     };
+
+  systemd.services.hardlink-storage = {
+    script = ''
+      hardlink -v -c /storage
+    '';
+    startAt = "monthly";
+    wantedBy = [ "multi-user.target" ];
+    path = with pkgs; [ util-linux ];
+  };
 }
