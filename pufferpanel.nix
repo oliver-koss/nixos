79352adbeb1202cbce7db2e32fa18c446bfb76a1
Maciej Krüger <mkg20001@gmail.com>
2024-07-29 23:05:40 +0200

    fix java

--- pufferpanel.nix
+++ pufferpanel.nix
@@ -2,6 +2,15 @@
 
 with lib;
 
+let
+  javaDrv = filterAttrs (n: p: (match "jdk[0-9]+" n != null) && !p.meta.insecure) pkgs;
+  javaBin = mapAttrsToList (name_: p: let 
+    name = replaceStrings [ "jdk" ] [ "java" ] name_;
+  in pkgs.runCommand name {} ''
+    mkdir -p $out/bin
+    ln -s ${p}/bin/java $out/bin/${name}
+  '') javaDrv;
+in
 {
   config = {
     services.nginx.virtualHosts = {
@@ -22,6 +31,8 @@ with lib;
         PUFFER_PANEL_REGISTRATIONENABLED = "false";
       };
     };
+
+    systemd.services.pufferpanel.path = javaBin;
 
     networking.firewall.allowedTCPPorts =  [ 8080 443 ];
   };
