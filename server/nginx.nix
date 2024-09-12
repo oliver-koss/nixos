e7ad6fd33f0487eff87003738b61d9bd45ff1d97
Oliver Koss <oliver.koss06@gmail.com>
2024-09-12 17:44:13 +0200

    setup uptime-kuma

---
+++ server/nginx.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+  h = a: a // {
+    enableACME = true;
+    forceSSL = true;
+  };
+in
+{
+  services.nginx.enable = true;
+
+  networking.firewall.allowedTCPPorts = [ 80 443 8080 ];
+  networking.firewall.allowedUDPPorts = [ 443 ];
+
+  services.nginx.virtualHosts = {
+    "uptime.oliver-koss.at" = h {
+      locations."/" = {
+        proxyPass = "http://localhost:8096/";
+      };
+    };
+  };
+
+}
