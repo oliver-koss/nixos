40eb532929cdf77a9b4c2fe022e0eed6c38ada15
Maciej Krüger <mkg20001@gmail.com>
2024-09-21 19:47:27 +0200

    torrent vpn leak prevention

--- torrent/ns.sh
+++ torrent/ns.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+n() {
+  n "$1"
+}
+
 case "$1" in
   up)
     ip l d tzouter || true
@@ -13,18 +17,22 @@ case "$1" in
       ip a a 10.0.7.1/24 dev tzouter
       ip l s tzouter up
 
-    ip netns exec tz /bin/sh -c "ip a a fd07::2/64 dev tzinner"
-      ip netns exec tz /bin/sh -c "ip a a 10.0.7.2/24 dev tzinner"
-      ip netns exec tz /bin/sh -c "ip l s tzinner up"
-      ip netns exec tz /bin/sh -c "ip r a dev tzinner default via 10.0.7.1"
-      ip netns exec tz /bin/sh -c "ip -6 r a dev tzinner default via fd07::1"
-      ip netns exec tz /bin/sh -c "ip r a dev tzinner 192.168.178.1 via 10.0.7.1"
-      ip netns exec tz /bin/sh -c "ip -6 r a dev tzinner fd83:e647:3f34:0::/64 via fd07::1"
+    n "ip a a fd07::2/64 dev tzinner"
+      n "ip a a 10.0.7.2/24 dev tzinner"
+      n "ip l s tzinner up"
+      # NOTE: please leave default rotes disabled
+      # this will otherwise lead to leaks when vpn fails in a way
+      # that's not caught by wg-quick
+      # n "ip r a dev tzinner default via 10.0.7.1"
+      # n "ip -6 r a dev tzinner default via fd07::1"
+      n "ip r a dev tzinner 192.168.178.1 via 10.0.7.1"
+      n "ip -6 r a dev tzinner fd83:e647:3f34:0::/64 via fd07::1"
+      n "ip -6 r a dev tzinner 2a09:7c47:0:15::1 via fd07::1"
 
-    ip netns exec tz /bin/sh -c "ip a a 127.0.0.1/8 dev lo"
-      ip netns exec tz /bin/sh -c "ip a a ::1/128 dev lo"
+    n "ip a a 127.0.0.1/8 dev lo"
+      n "ip a a ::1/128 dev lo"
 
-    ip netns exec tz /bin/sh -c "wg-quick up /var/pq.conf"
+    n "wg-quick up /var/pq.conf"
 	;;
   down)
     ip l d tzouter
