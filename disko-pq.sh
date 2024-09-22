d5aca1c4a572578a545735e4d85b8cbf38de4c17
Maciej Krüger <mkg20001@gmail.com>
2024-09-22 15:06:57 +0200

    update pq instructions

--- disko-pq.sh
+++ disko-pq.sh
@@ -3,12 +3,25 @@
 set -euxo pipefail
 # current disko is broken so we use a pinned commit
 nix run github:nix-community/nixos-anywhere/69ad3f4a50cfb711048f54013404762c9a8e201e -- --option pure-eval "false" --flake .#pq-vpn root@pq.furrytel.me
+
+
 # instructions:
 # go to pq.hosting
 # login
 # virtual private servers
 # select three dot menu next to server -> click go to panel
 # on panel select three dot menu -> mount iso image
+
+# WITH PATCHED ISO
+# select public url, enter https://mkg20001.io/tmp/pq.iso
+# wait until machine boots (ping ip)
+# run disk-pq.sh
+
+# BUILD & UPLOAD PATCHED ISO
+# nix build .#pq-iso
+# rsync result/*/*iso helium.y.mkg20001.io:/srv/www/pq.iso --progress
+
+# WITH REGULAR NIXOS ISO
 # select public url, enter https://channels.nixos.org/nixos-24.05/latest-nixos-minimal-x86_64-linux.iso
 # wait
 # go to vnc
