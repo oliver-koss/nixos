133d027cb5b9e5e4bfb35a4c8ac398dbb51ab373
Oliver Koss <oliver.koss06@gmail.com>
2024-10-17 00:15:52 +0200

    add desktop publickey

--- config.nix
+++ config.nix
@@ -21,7 +21,7 @@ with lib;
       ./incus.nix
 #      ./backup-rar.nix
       ./cli-tools.nix
-      ./i3wm.nix
+#      ./i3wm.nix
     ];
 
 #  services.wordpress.sites."kai" = {};
@@ -66,7 +66,6 @@ with lib;
   virtualisation.incus.enable = true;
   virtualisation.incus.ui.enable = true;
 
-/*
   # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
@@ -80,8 +79,6 @@ with lib;
     wayland.enable = true;
 #    theme = "reactionary";
   };
-
-*/
 
 #  services.xserver.displayManager.sddm.settings = {
 #    Theme = {
