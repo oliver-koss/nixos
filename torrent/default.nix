42a7d9d913fcfa55a51982e46323b628c5322c13
Maciej Krüger <mkg20001@gmail.com>
2024-12-06 22:08:50 +0100

    bla

--- torrent/default.nix
+++ torrent/default.nix
@@ -7,6 +7,13 @@
     ./sabnzbd.nix
   ];
 
+              nixpkgs.config.permittedInsecurePackages = [
+                "aspnetcore-runtime-wrapped-6.0.36"
+                "aspnetcore-runtime-6.0.36"
+                "dotnet-sdk-wrapped-6.0.428"
+                "dotnet-sdk-6.0.428"
+              ];
+
   security.acme.distributor-server = "https://acme.s.xeredo.it";
   security.acme.distributor-token = "esti75v4kzb3i5viznw3it5w4zv5j3wi5v";
 }
