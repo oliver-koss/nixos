dea8d2d5a3b2aac1aa5d54fe03c329951cd869d3
Maciej Krüger <mkg20001@gmail.com>
2025-01-18 19:09:02 +0100

    nuc memtest

--- nuc.nix
+++ nuc.nix
@@ -54,6 +54,8 @@ with lib;
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.memtest86.enable = true;
+  boot.loader.systemd-boot.netbootxyz.enable = true;
 
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
