586c59da5172914f6b5e6dd6a0f173691e355964
Maciej Krüger <mkg20001@gmail.com>
2025-04-02 03:56:36 +0200

    replace vpn ip script

---
+++ replace-vpn-ip.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+NEWV4="$1"
+NEWV6="$2"
+
+OLDV4=$(cat pq/values.nix  | grep ipv4 | grep -o "[0-9][0-9][0-9.]*")
+OLDv6=$(cat pq/values.nix  | grep ipv6 | grep -o "[0-9][0-9a-f][0-9a-f][0-9a-f:]*")
+
+SELF=$(dirname $(readlink -f "$0"))
+
+replace()  {
+  find "$SELF/pq" "$SELF/torrent" -type f -exec sed -i "s|$1|$2|g" {} \;
+}
+
+replace "$OLDV4" "$NEWV4"
+replace "$OLDV6" "$NEWV6"
