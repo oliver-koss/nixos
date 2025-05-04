cf8287a7b1485cd8aa81d812165c7e525f40cf4a
Maciej Krüger <mkg20001@gmail.com>
2025-05-04 18:06:18 +0200

    rdp comments

--- nuc/rdp.nix
+++ nuc/rdp.nix
@@ -1,3 +1,30 @@
+# more context: https://discourse.nixos.org/t/configuring-remote-desktop-access-with-gnome-remote-desktop/48023/4?u=mkg20001
+
+/*
+Run after install:
+
+sudo systemctl restart gnome-remote-desktop.service
+sudo grdctl --system rdp enable
+
+sudo rm -rf ~gnome-remote-desktop/rdp-tls*
+sudo -u gnome-remote-desktop winpr-makecert     -silent -rdp -path ~gnome-remote-desktop rdp-tls
+
+sudo grdctl --system rdp set-tls-key /var/lib/gnome-remote-desktop/rdp-tls.key
+sudo grdctl --system rdp set-tls-cert /var/lib/gnome-remote-desktop/rdp-tls.crt
+sudo grdctl --system rdp set-tls-cert /var/lib/gnome-remote-desktop/rdp-tls.crt
+sudo grdctl --system rdp set-tls-cert /var/lib/gnome-remote-desktop/rdp-tls.crt
+sudo grdctl --system rdp set-tls-key /var/lib/gnome-remote-desktop/rdp-tls.key
+
+sudo systemctl daemon-reload
+sudo systemctl enable --now gnome-remote-desktop.service
+sudo grdctl --system rdp set-credentials "name" "password"
+
+sudo systemctl restart gnome-remote-desktop.service
+sudo systemctl status gnome-remote-desktop.service`
+
+sudo grdctl --system rdp enable
+sudo grdctl --system status
+*/
 { pkgs, config, lib, ... }: {
   services.xserver = {
     enable = true;
