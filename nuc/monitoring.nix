21b9c49efbe66b9cde8b7c148ce865dc1b870fde
Oliver Koss <oliver.koss06@gmail.com>
2025-12-13 13:53:26 +0100

    update monitoring

--- nuc/monitoring.nix
+++ nuc/monitoring.nix
@@ -3,6 +3,32 @@
     services.prometheus = {
         enable  = true;
         port    = 9090;
+        scrapeConfigs = [
+            {
+                job_name = "node";
+                static_configs = [{
+                    targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+                }];
+            }
+        ];
+    };
+
+    services.prometheus.exporters.node = {
+        enable = true;
+        port = 9000;
+        listenAddress = "localhost";
+        # For the list of available collectors, run, depending on your install:
+        # - Flake-based: nix run nixpkgs#prometheus-node-exporter -- --help
+        # - Classic: nix-shell -p prometheus-node-exporter --run "node_exporter --help"
+        enabledCollectors = [
+        "ethtool"   
+        "softirqs"
+        "systemd"
+        "tcpstat"
+        "wifi"
+        ];
+
+        extraFlags = [ "--collector.ntp.protocol-version=4" "--no-collector.mdadm" ];
     };
 
     services.grafana = {
@@ -23,28 +49,5 @@
       adminUser = "admin";
     };
     };
-
-  # https://nixos.org/manual/nixos/stable/#module-services-prometheus-exporters
-  # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/services/monitoring/prometheus/exporters.nix
-  services.prometheus.exporters.node = {
-    enable = true;
-    port = 9000;
-    listenAddress = "localhost";
-    # For the list of available collectors, run, depending on your install:
-    # - Flake-based: nix run nixpkgs#prometheus-node-exporter -- --help
-    # - Classic: nix-shell -p prometheus-node-exporter --run "node_exporter --help"
-    enabledCollectors = [
-      "ethtool"   
-      "softirqs"
-      "systemd"
-      "tcpstat"
-      "wifi"
-    ];
-    # You can pass extra options to the exporter using `extraFlags`, e.g.
-    # to configure collectors or disable those enabled by default.
-    # Enabling a collector is also possible using "--collector.[name]",
-    # but is otherwise equivalent to using `enabledCollectors` above.
-    extraFlags = [ "--collector.ntp.protocol-version=4" "--no-collector.mdadm" ];
-  };
 
 }
