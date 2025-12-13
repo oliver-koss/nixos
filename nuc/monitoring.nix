3f2de0d1b3117b00ea627cb3c765ba24bfe6914e
oliver-koss <oliver.koss06@gmail.com>
2025-12-13 13:26:57 +0100

    update grafana

--- nuc/monitoring.nix
+++ nuc/monitoring.nix
@@ -10,7 +10,7 @@
     settings = {
         server = {
         # Listening Address
-        http_addr = "127.0.0.1";
+        http_addr = "0.0.0.0";
         # and Port
         http_port = 3000;
         # Grafana needs to know on which domain and URL it's running
