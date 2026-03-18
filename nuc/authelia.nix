04bdb19c155a0e61d06c2fa2b4cb7fb2b8186e7b
oliver-koss <oliver.koss06@gmail.com>
2026-03-18 15:19:13 +0100

    update authelia config

--- nuc/authelia.nix
+++ nuc/authelia.nix
@@ -2,37 +2,41 @@
 
 #networking.firewall.allowedTCPPorts = [ 9091 ];
 
-services.authelia.instances = {
-
-  main = {
-    enable = false;
-    secrets.storageEncryptionKeyFile = "/etc/authelia/storageEncryptionKeyFile";
-    secrets.jwtSecretFile = "/etc/authelia/jwtSecretFile";
-    settings = {
-      theme = "light";
-      default_2fa_method = "totp";
-      log.level = "debug";
-      server.disable_healthcheck = true;
-      authentication_backend.file = {
-        path = "/etc/authelia/users.yml";
-      };
-      storage.local.path = "/etc/authelia/db.sqlite";
-      session = {
-        domain = "sso.oliver-koss.at";
-        expiration = "3600";
-        inactivity = "300";
-      };
-      notifier.filesystem.filename = "/etc/authelia/emails.txt";
-      access_control = {
-        default_policy = "deny";
-        rules = {
-          domain = "*.oliver-koss.at";
-          policy = "one_factor";
+  services.authelia.instances = {
+    main = {
+      enable = true;
+      secrets.storageEncryptionKeyFile = "/var/lib/authelia-main/storageEncryptionKeyFile";
+      secrets.jwtSecretFile = "/var/lib/authelia-main/jwtSecretFile";
+      settings = {
+        theme = "light";
+        default_2fa_method = "totp";
+        log.level = "debug";
+        server.disable_healthcheck = true;
+        authentication_backend.file = {
+          path = "/var/lib/authelia-main/users.yml";
+        };
+        storage.local.path = "/var/lib/authelia-main/db.sqlite";
+        session = {
+          cookies = [
+            {
+              domain = "oliver-koss.at";
+              authelia_url = "https://sso.oliver-koss.at";
+              expiration = "3600";
+              inactivity = "300";
+            }
+          ];
+        };
+        notifier.filesystem.filename = "/var/lib/authelia-main/emails.txt";
+        access_control = {
+          default_policy = "deny";
+          rules = [
+            {
+              domain = "*.oliver-koss.at";
+              policy = "one_factor";
+            }
+          ];
         };
       };
     };
   };
-
-};
-
 }
